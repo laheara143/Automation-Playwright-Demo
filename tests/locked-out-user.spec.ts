ea59e5b0035971ee6074d4f32c165d18eb367bbf
Hannah Cameron <77558582+laheara143@users.noreply.github.com>
2026-04-05 21:30:38 +0000

    Adding in script for locked out user and documentation for standard user

---
+++ tests/locked-out-user.spec.ts
@@ -0,0 +1,30 @@
+import {test , expect} from '@playwright/test' ;
+
+test.describe('Locked Out User cannot sign in and recieves an error message', () => {
+
+    test.beforeEach(async ({page}) =>{
+
+        await page.goto('https://www.saucedemo.com/');
+    });
+
+    test('Attempt to log in and verify error message ', async ({page}) => {
+
+        //Verify reponse of site
+        const response = await page.goto('https://www.saucedemo.com/');
+        expect(response?.status()).toBe(200);
+
+        //Fill in log in information and click 'Login'
+        await page.fill('[data-test="username"]', 'locked_out_user');
+        await page.fill('[data-test="password"]', 'secret_sauce');
+        await page.click('[data-test="login-button"]');
+
+        //Verify Unsuccesful Login
+        await expect (page).toHaveURL(/www.saucedemo.com/);
+
+        //Verify Error Message
+        await expect (page.locator('[data-test="error"]')).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+
+    });
+
+
+});
--- tests/standard-user.spec.ts
+++ tests/standard-user.spec.ts
@@ -1,6 +1,6 @@
 import {test , expect } from '@playwright/test';
 
-test.describe('Demo Script', () => {
+test.describe('Standard User can add items and complete checkout', () => {
 
 test.beforeEach(async ({page}) => {
     await page.goto('https://www.saucedemo.com/');
@@ -8,31 +8,41 @@ test.beforeEach(async ({page}) => {
 
 test('Navigate to page, Log In, Add Items, Checkout', async ({page}) => {
 
+    //Navigate to site and login
     const response = await page.goto('https://www.saucedemo.com/');
-
     await page.fill('[data-test="username"]' , 'standard_user');
     await page.fill('[data-test="password"]' , 'secret_sauce');
     await page.click('[data-test="login-button"]');
 
+    //Wait for page to fully load and verify response
     await page.waitForLoadState();
     expect(response?.status()).toBe(200);
 
+    //Add items to cart
     await page.click('[data-test="add-to-cart-sauce-labs-backpack"]');
     await page.click('[data-test="add-to-cart-sauce-labs-bike-light"]');
+
+    //Verify cart badge is updated
     await expect (page.locator('[data-test="shopping-cart-badge"]')).toHaveText('2');
+
+    //Go to cart
     await page.click('#shopping_cart_container');
 
+    //Go to Checkout
     await page.click('[data-test="checkout"]');
 
+    //Fill in Checkout Information
     await page.fill('[data-test="firstName"]' , 'Test');
     await page.fill('[data-test="lastName"]' , '1');
     await page.fill('[data-test="postalCode"]' , '12345');
-    await page.click('[data-test="continue"]');
 
+    //Go to overview page and verify price
+    await page.click('[data-test="continue"]');
     await expect (page).toHaveURL(/checkout-step-two.html/);
     await expect (page.locator('[data-test="title"]')).toHaveText('Checkout: Overview');
     await expect (page.locator('[data-test="total-label"]')).toHaveText('Total: $43.18');
 
+    //Confirm order and verify completion page
     await page.click('[data-test="finish"]');
     await expect (page).toHaveURL(/checkout-complete.html/);
 
