91a0030955b6757f8dda23d38b3f8707ba7bb056
Hannah Cameron <77558582+laheara143@users.noreply.github.com>
2026-04-09 21:44:39 +0000

    Adding in POM for Inventory Page

---
+++ pages/inventoryPage.ts
@@ -0,0 +1,22 @@
+import { Page } from '@playwright/test';
+
+export class InventoryPage {
+    constructor(private page: Page) {}
+
+    async addItemToCart(item: string) {
+        await this.page.locator('[data-test="add-to-cart-${item}"]').click();
+    }
+
+    async removeItem(item: string) {
+        await this.page.locator('[data-test="remove-${item}"]').click();
+    }  
+
+    async goToCart() {
+        await this.page.locator('#shopping_cart_container').click();
+    }    
+
+    async applyFilter(value: string) {
+        await this.page.selectOption('.product-sort-container', value);
+    }    
+
+}
