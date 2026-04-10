50f94852a3eb1eeca6007c67e615fd0ce4328619
Hannah Cameron <77558582+laheara143@users.noreply.github.com>
2026-04-10 22:14:12 +0000

    Updating cartPage.ts file to have timeout added to verifyCartAmount

--- pages/cartPage.ts
+++ pages/cartPage.ts
@@ -4,7 +4,7 @@ export class CartPage {
     constructor(private page: Page) {}
 
     async verifyItemInCart(itemName: string) {
-        await expect (this.page.locator('.inventory_item_name')).toContainText(itemName);
+        await expect (this.page.locator('.inventory_item_name', { hasText: itemName})).toBeVisible();
     }
 
     async removeItem(itemName: string) {
@@ -13,6 +13,12 @@ export class CartPage {
 
     async verifyCartEmpty() {
         await expect (this.page.locator('.cart_item')).toHaveCount(0);
-    }  
+    } 
     
+    async verifyCartAmount(itemCount: number) {
+        await expect (this.page.locator('.cart_item')).toHaveCount(itemCount, {
+        timeout: 5000
+
+       });
+    }     
 }
