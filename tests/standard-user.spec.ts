0e0901e0bc5786044d15e64904028405311d25a7
Hannah Cameron <77558582+laheara143@users.noreply.github.com>
2026-04-13 20:13:01 +0000

    Adding assertions for checkoutPage POM file

--- tests/standard-user.spec.ts
+++ tests/standard-user.spec.ts
@@ -2,6 +2,7 @@ import {test , expect } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
 import { InventoryPage } from '../pages/inventoryPage';
 import { CartPage } from '../pages/cartPage';
+import { CheckoutPage } from '../pages/checkoutPage';
 
 test.describe('Standard User can add items and complete checkout', () => {
 
@@ -14,6 +15,7 @@ test.beforeEach(async ({page}) => {
 
 test('Navigate to page, Log In, Add Items, Checkout', async ({page}) => {
 
+    const checkoutPage = new CheckoutPage(page);
     const inventoryPage = new InventoryPage(page);
     const cartPage = new CartPage(page);
 
@@ -34,21 +36,18 @@ test('Navigate to page, Log In, Add Items, Checkout', async ({page}) => {
     await cartPage.verifyCartAmount(2);
 
     //Go to Checkout
-    await page.click('[data-test="checkout"]');
+    await checkoutPage.goToCheckout();
 
     //Fill in Checkout Information
-    await page.fill('[data-test="firstName"]' , 'Test');
-    await page.fill('[data-test="lastName"]' , '1');
-    await page.fill('[data-test="postalCode"]' , '12345');
+    await checkoutPage.fillCheckoutInfo('Tester', 'Test', '12345');
 
     //Go to overview page and verify price
-    await page.click('[data-test="continue"]');
     await expect (page).toHaveURL(/checkout-step-two.html/);
     await expect (page.locator('[data-test="title"]')).toHaveText('Checkout: Overview');
     await expect (page.locator('[data-test="total-label"]')).toHaveText('Total: $43.18');
 
     //Confirm order and verify completion page
-    await page.click('[data-test="finish"]');
+    await checkoutPage.finishCheckout();
     await expect (page).toHaveURL(/checkout-complete.html/);
 
     //Screenshot to verify Test Results
